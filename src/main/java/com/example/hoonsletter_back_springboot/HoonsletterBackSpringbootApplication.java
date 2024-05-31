16387c74e42b22253475f843db810dbf1653261f
rkdeofuf <asa47972000@naver.com>
2024-05-31 18:33:15 +0900

    springboot project init

---
+++ src/main/java/com/example/hoonsletter_back_springboot/HoonsletterBackSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.hoonsletter_back_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoonsletterBackSpringbootApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HoonsletterBackSpringbootApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/hoonsletter_back_springboot/HoonsletterBackSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hoonsletter_back_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoonsletterBackSpringbootApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

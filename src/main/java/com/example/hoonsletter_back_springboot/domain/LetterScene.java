9fd609fcfa48ed8761a2c5c8bcd7fd4a4e3493d1
rkdeofuf <asa47972000@naver.com>
2024-06-13 23:06:50 +0900

    add LetterScene Entity

---
+++ src/main/java/com/example/hoonsletter_back_springboot/domain/LetterScene.java
@@ -0,0 +1,64 @@
+package com.example.hoonsletter_back_springboot.domain;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import java.util.List;
+import java.util.Objects;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+public class LetterScene {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @Column(nullable = false)
+  private int order;
+
+  // letter_scene -> letter_message 단방향 매핑
+  @ToString.Exclude
+  @JoinColumn(name = "scene_id")
+  @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE, orphanRemoval = true)
+  private List<SceneMessage> sceneMessages;
+
+  // letter_scene -> letter_picture 단방향 매핑
+  @ToString.Exclude
+  @JoinColumn(name = "scene_id")
+  @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE, orphanRemoval = true)
+  private List<ScenePicture> scenePictures;
+
+  protected LetterScene(){} // no-args constructor
+
+  private LetterScene(int order){
+    this.order = order;
+  }
+
+  public static LetterScene of(int order){
+    return new LetterScene(order);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if(this == o) return true;
+    if(!(o instanceof LetterScene that)) return false;
+
+    return getId() != null && getId().equals(that.getId());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(getId());
+  }
+}

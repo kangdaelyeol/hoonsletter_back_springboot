977c31db1cb9493aa593d25f70c7936589a85f27
rkdeofuf <asa47972000@naver.com>
2024-07-01 20:30:21 +0900

    add pagination service
    페이지네이션 바 정보 제공을 위한 PaginationService 구현 및 테스트 코드 추가.
    임의로 페이지바의 길이는 5로 정함.
    ParameterizedTest로 여러 경우의 수를 둔 테스트 작성 및 통과

---
+++ src/main/java/com/example/hoonsletter_back_springboot/service/PaginationService.java
@@ -0,0 +1,21 @@
+package com.example.hoonsletter_back_springboot.service;
+
+
+import java.util.List;
+import java.util.stream.IntStream;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaginationService {
+  public static final int BAR_LENGTH = 5;
+
+  public List<Integer> getPaginationBarNumbers(int currentPageNumber, int totalPages) {
+    int startNumber = Math.max(currentPageNumber - (BAR_LENGTH / 2), 0);
+    int endNumber = Math.min(startNumber + BAR_LENGTH, totalPages);
+    return IntStream.range(startNumber, endNumber).boxed().toList();
+  }
+
+  public int currentBarLength() {
+    return BAR_LENGTH;
+  }
+}

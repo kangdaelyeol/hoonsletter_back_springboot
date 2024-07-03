d32b6592712e63d77a1ff4a06c7419836dec08f2
rkdeofuf <asa47972000@naver.com>
2024-07-03 21:28:48 +0900

    add SecurityConfig
    해시를 도와주는 passwordEncoder를 사용하기 위해 추가

---
+++ src/main/java/com/example/hoonsletter_back_springboot/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.example.hoonsletter_back_springboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+  }
+
+}

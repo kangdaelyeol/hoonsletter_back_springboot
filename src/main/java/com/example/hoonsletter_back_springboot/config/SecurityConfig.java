c5b07e51aba0ca99a41032dab908bcd2e767381b
rkdeofuf <asa47972000@naver.com>
2024-09-03 19:12:38 +0900

    update Security Config
    FE에서 formData를 보내고 응답을 받을 때 Cors때문에 받을 수 없어서 임시적으로 모든 접근에 대해 허용해줌.

--- src/main/java/com/example/hoonsletter_back_springboot/config/SecurityConfig.java
+++ src/main/java/com/example/hoonsletter_back_springboot/config/SecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -28,6 +30,19 @@ public class SecurityConfig {
     return httpSecurity
         .httpBasic(AbstractHttpConfigurer::disable) // (customizer -> customizer.disable()) (same)
         .csrf(AbstractHttpConfigurer::disable)
+        .cors(cors -> {
+          CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+          corsConfiguration.addAllowedOrigin("*");
+          corsConfiguration.addAllowedMethod("*");
+          corsConfiguration.addAllowedHeader("*");
+
+          UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+          source.registerCorsConfiguration("/**", corsConfiguration);
+
+          cors.configurationSource(source);
+        })
         .formLogin(AbstractHttpConfigurer::disable)
         .sessionManagement((customizer) ->
           customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
